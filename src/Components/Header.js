b57dc95c6ace5dc3711a28df298c1e7a392103d4
Ilya Choroszun <choroshun01@gmail.com>
2022-05-07 17:07:05 +0200

    first stage of main menu

---
+++ src/Components/Header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Navbar, Nav, Form, FormControl, Container, Button } from 'react-bootstrap'
+import logo from './lecha.png'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <Navbar collapseOneSelect  expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/" >
+                        <img
+                            src={logo}
+                            height="30"
+                            width="30"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" >
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/"> Home </Nav.Link>
+                            <Nav.Link href="/about"> About us </Nav.Link>
+                            <Nav.Link href="/contacts"> Contacts </Nav.Link>
+                            <Nav.Link href="/blog"> Blog </Nav.Link>
+                        </Nav>
+                        <Form class="form-inline">
+                            <FormControl
+                                type="text"
+                                placeholder="Search"
+                                className="mr-sm-2"
+                            />
+                            <Button variant="outline-info">UwU</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+     } 
+}
